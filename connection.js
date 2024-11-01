098ba11e93ceb0869b868764ff58060a8c41a220
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-11-01 13:09:01 -0300

    Add offer and answer creation

--- connection.js
+++ connection.js
@@ -8,15 +8,44 @@ const button = document.getElementById("connection-button");
 const message = document.getElementById("connection-message");
 
 // Connection
-const [offer, answer] = getOfferAndAnswer();
+const connection = await initializeConnection();
+const channel = connection.createDataChannel("data");
+channel.onmessage((event) => alert(event.data));
 
-// TODO: if all arguments are present, connect
+async function makeOffer() {
+  const offer = connection.createOffer();
+  await connection.setLocalDescription(offer);
+}
 
-// TODO: create initiation link
+async function makeAnswer() {
+  if (connection.remoteDescription) {
+    const answer = await connection.createAnswer();
+    await connection.setLocalDescription(answer);
+  }
+}
 
-// TODO: create response linkk
+// TODO: generate a link
+function makeLink() {}
 
 // Functions
+async function initializeConnection() {
+  const connection = new RTCPeerConnection(RTC_CONNECTION_SETTINGS);
+  const [offer, answer] = getOfferAndAnswer();
+
+  if (offer) {
+    if (answer) {
+      // This is the offering party replying to the answer
+      await connection.setLocalDescription(offer);
+      await connection.setRemoteDescription(answer);
+    } else {
+      // This is the answering party
+      await connection.setRemoteDescription(offer);
+    }
+  }
+
+  return connection;
+}
+
 function getOfferAndAnswer() {
   const params = window.location.search.slice(1).split("&");
 
