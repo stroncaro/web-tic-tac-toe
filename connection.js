ef5ebc3fa824d53eb3b4fc4f6548f977b3514101
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-11-01 18:05:39 -0300

    Add connection event handlers

--- connection.js
+++ connection.js
@@ -7,43 +7,77 @@ const RTC_CONNECTION_SETTINGS = {
 const button = document.getElementById("connection-button");
 const message = document.getElementById("connection-message");
 
-// Connection
-const connection = await initializeConnection();
-const channel = connection.createDataChannel("data");
-channel.onmessage((event) => alert(event.data));
+// Connect and set up
+let channel = null;
+const connection = new RTCPeerConnection(RTC_CONNECTION_SETTINGS);
+let [offer, answer] = getOfferAndAnswer();
 
-async function makeOffer() {
-  const offer = connection.createOffer();
+connection.ondatachannel = (event) => {
+  console.log("ondatachannel");
+  channel = event.channel;
+  channel.onmessage = (event) => alert(event.data);
+};
+
+connection.onconnectionstatechange = (event) => {
+  console.log(`connectionstatechange: ${connection.connectionState}`);
+};
+
+connection.oniceconnectionstatechange = (event) => {
+  console.log(`iceconnectionstatechange: ${connection.iceConnectionState}`);
+};
+
+(async () => {
+  if (offer) {
+    if (answer) {
+      // This is the offering party receiving the answer
+      await applyOffer(offer);
+      await acceptAnswer(answer);
+
+      // TODO: show feedback
+    } else {
+      // This is the answering party receiving an offer
+      await acceptRemoteOffer(offer);
+
+      // TODO: show feedback
+    }
+  } else {
+    message.textContent = "Click button to start connection process";
+    button.disabled = false;
+    button.onclick = createOffer;
+  }
+})();
+
+// Functions
+async function createOffer() {
+  channel = connection.createDataChannel("data");
+  channel.onmessage = (event) => alert(event.data);
+
+  offer = await connection.createOffer();
+  await connection.setLocalDescription(offer);
+
+  // TODO: create link
+}
+
+async function applyOffer(offer) {
+  channel = connection.createDataChannel("data");
+  channel.onmessage = (event) => alert(event.data);
+
   await connection.setLocalDescription(offer);
 }
 
-async function makeAnswer() {
-  if (connection.remoteDescription) {
-    const answer = await connection.createAnswer();
-    await connection.setLocalDescription(answer);
-  }
+async function acceptRemoteOffer(offer) {
+  await connection.setRemoteDescription(offer);
 }
 
-// TODO: generate a link
-function makeLink() {}
+async function createAnswer() {
+  answer = await connection.createAnswer();
+  await connection.setLocalDescription(answer);
 
-// Functions
-async function initializeConnection() {
-  const connection = new RTCPeerConnection(RTC_CONNECTION_SETTINGS);
-  const [offer, answer] = getOfferAndAnswer();
+  // TODO: create link
+}
 
-  if (offer) {
-    if (answer) {
-      // This is the offering party replying to the answer
-      await connection.setLocalDescription(offer);
-      await connection.setRemoteDescription(answer);
-    } else {
-      // This is the answering party
-      await connection.setRemoteDescription(offer);
-    }
-  }
-
-  return connection;
+async function acceptAnswer(answer) {
+  await connection.setRemoteDescription(answer);
 }
 
 function getOfferAndAnswer() {
